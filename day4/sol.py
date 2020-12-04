fb7cc528c8131ebad917f812bc898d2c5c331acf
jartigag <jartigag@pm.me>
2020-12-04 11:54:07 +0100

    dict comprehension

--- day4/sol.py
+++ day4/sol.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 input=open("input").read().split("\n\n")
+input = [dict(kv.split(':') for kv in line.split()) for line in input]
 
 def validate_minmax(val,min,max,digits=0):
     base=16 if any(c in val for c in 'abcdef') else 10
@@ -12,29 +13,26 @@ def validate_minmax(val,min,max,digits=0):
             return True
     return False
 
-def get_val(field,line):
-    return line.split(field)[1].split()[0]
-
 valid=0
 
 for passport in input:
 
-    if all(f in passport for f in ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']):
+    if all(f in passport for f in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']):
 
         #valid+=1; continue #solution1
 
-        if      validate_minmax(get_val('byr:',passport),1920,2002,4) \
-            and validate_minmax(get_val('iyr:',passport),2010,2020,4) \
-            and validate_minmax(get_val('eyr:',passport),2020,2030,4):
+        if      validate_minmax(passport['byr'],1920,2002,4) \
+            and validate_minmax(passport['iyr'],2010,2020,4) \
+            and validate_minmax(passport['eyr'],2020,2030,4):
 
-            if         ( 'cm'==get_val('hgt:',passport)[-2:] and validate_minmax(get_val('hgt:',passport)[:-2],150,193) ) \
-                    or ( 'in'==get_val('hgt:',passport)[-2:] and validate_minmax(get_val('hgt:',passport)[:-2],59,76) ):
+            if         ( 'cm'==passport['hgt'][-2:] and validate_minmax(passport['hgt'][:-2],150,193) ) \
+                    or ( 'in'==passport['hgt'][-2:] and validate_minmax(passport['hgt'][:-2],59,76) ):
 
-                if '#'==get_val('hcl:',passport)[0] and validate_minmax(get_val('hcl:',passport)[1:],0x000000,0xffffff,6):
+                if '#'==passport['hcl'][0] and validate_minmax(passport['hcl'][1:],0x000000,0xffffff,6):
 
-                    if any(c in get_val('ecl:',passport) for c in ['amb','blu','brn','gry','grn','hzl','oth']):
+                    if any(c in passport['ecl'] for c in ['amb','blu','brn','gry','grn','hzl','oth']):
 
-                        if get_val('pid:',passport).isnumeric() and validate_minmax(get_val('pid:',passport),0,999999999,9):
+                        if passport['pid'].isnumeric() and validate_minmax(passport['pid'],0,999999999,9):
 
                             valid+=1
 
